42f139e8fdfbf5380ab5ca1d6d4200a071d0841a
Hasan3773 <h2tahir@uwaterloo.ca>
2024-03-19 21:36:06 -0400

    Time: 12 ms (61.53%), Space: 14 MB (28.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> table;
-        vector<int> answer;
-
-        for (int i = 0; i < nums.size(); i++){
-            int compliment = target - nums[i];
-            if (table.count(compliment) > 0){
-                return {table[compliment],i};
-            }   
-            table[nums[i]] = i;
+        // given an array of nums and a target 
+        // return two nums that add up to target 
+        #include <unordered_map> 
+        unordered_map<int,int> map;
+        
+        for (int i = 0; i < nums.size(); i++){        
+            // calculate the value that we are looking for
+            int value = target - nums[i];
+            
+            // go through the current map looking for our value
+            if(map.find(value) != map.end()){
+                return {map[value],i};
+            }  
+            map.insert(make_pair(nums[i], i));    
         }
-        return {};
-
+        return vector<int>{0};
     }
 };
