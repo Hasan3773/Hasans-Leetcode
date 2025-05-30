a8c02d49ea514aa791d5a8de478ed08d81c253ce
Hasan Tahir <h2tahir@uwaterloo.ca>
2025-05-30 16:58:38 -0400

    Time: 235 ms (66.67%), Space: 25.9 MB (94.44%) - LeetHub

---
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def closestMeetingNode(self, edges, node1, node2):
+        """
+        :type edges: List[int]
+        :type node1: int
+        :type node2: int
+        :rtype: int
+        """
+        def bfs(startNode, edges, dist):
+            # perform traversal 
+            # dist stores shortest distance from start node to all other nodes
+            n = len(edges)
+            queue = []
+            queue.append(startNode)
+
+            visited = [0] * n # set all to false
+            dist[startNode] = 0 # what
+
+            while queue:
+                currentNode = queue.pop()
+                if visited[currentNode]:
+                    continue
+                visited[currentNode] = True
+                neighbor = edges[currentNode]
+                if (neighbor != -1 and not visited[neighbor]):
+                    dist[neighbor] = 1 + dist[currentNode] # what
+                    queue.append(neighbor)
+        dist1 = [float('inf')] * len(edges)
+        dist2 = [float('inf')] * len(edges)
+        n = len(edges)
+
+        bfs(node1, edges, dist1)
+        bfs(node2, edges, dist2)
+
+        minDistNode = -1
+        minDistTillNow = float('inf')
+
+        for currentNode in range(n):
+            if (minDistTillNow > max(dist1[currentNode], dist2[currentNode])):
+                minDistNode = currentNode
+                minDistTillNow = max(dist1[currentNode], dist2[currentNode])
+        return minDistNode
+        
