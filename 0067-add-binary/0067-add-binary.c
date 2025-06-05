cb9655afbc58cc40b4d2c3c99c2b8bc1a405194a
Hasan Tahir <h2tahir@uwaterloo.ca>
2025-06-05 19:33:56 -0400

    Time: 0 ms (100%), Space: 8 MB (67.16%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.c
@@ -0,0 +1,25 @@
+#define max(x,y) (((x) >= (y)) ? (x) : (y))
+
+char* addBinary(char* a, char* b) {
+    int a_len = strlen(a);
+    int b_len = strlen(b);
+    int size = max(a_len, b_len);
+    char* output = (char *) malloc(size + 2); // one for overflow one for \0
+    output[size + 1] = '\0';
+    int i = a_len - 1;
+    int j = b_len - 1;
+    int k = size;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry){
+        int bitA = (i >= 0) ? a[i--] - '0': 0;
+        int bitB = (j >= 0) ? b[j--] - '0': 0;
+        int sum = bitA ^ bitB ^ carry;
+        carry = (bitA & bitB) | (bitA & carry) | (bitB & carry);
+        output[k--] = sum + '0';
+    }
+    if (k == -1) return output;
+    return output + k + 1;
+    
+    return output; 
+}
