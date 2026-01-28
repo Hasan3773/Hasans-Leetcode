78d520665c1bb2562b9aae8cbf54364dbf6e3d32
Hasan Tahir <h2tahir@uwaterloo.ca>
2026-01-28 11:49:47 -0500

    Time: 0 ms (100%), Space: 21.8 MB (37.23%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int x = 0;
+
+        for (int i = 0; i <= nums.size(); i++){
+            x ^= i;
+        }
+        for (int i = 0; i < nums.size(); i++){
+            x ^= nums[i];
+        }
+        return x; 
+    }
+};
